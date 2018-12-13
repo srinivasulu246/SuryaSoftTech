cf15231024d61972cdede154431a785816ce31dd
Praveen Bharath Jangre <praveen.jangre@wolterskluwer.com>
2018-12-13 10:58:27 +0530

    Initial Commit

---
+++ CoreData1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData1
+//
+//  Created by Srinivas on 13/12/18.
+//  Copyright © 2018 impelsys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
